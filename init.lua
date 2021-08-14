c4f98aeaed2da7328eec4fe3890b6c31936d5f34
Omar Monrroy <45756472+Drk01@users.noreply.github.com>
2021-08-14 18:28:50 -0500

    Formatting code

--- init.lua
+++ init.lua
@@ -6,13 +6,17 @@ vim.g.onedark_terminal_italics = 2
 vim.cmd [[colorscheme onedark]]
 vim.cmd [[highlight link CompeDocumentation NormalFloat]]
 
-vim.g.t_Co=256
+vim.g.t_Co = 256
 
 -- Lightline
 vim.g.lightline = {
-  colorscheme = 'onedark',
-  active = { left = { { 'mode', 'paste' }, { 'gitbranch', 'readonly', 'filename', 'modified' } } },
-  component_function = { gitbranch = 'fugitive#head' },
+    colorscheme = 'onedark',
+    active = {
+        left = {
+            {'mode', 'paste'}, {'gitbranch', 'readonly', 'filename', 'modified'}
+        }
+    },
+    component_function = {gitbranch = 'fugitive#head'}
 }
 
 -- Autoclose tags
