2fcbd07553fb024e0167544f76279c872463c517
Omar Monrroy <45756472+Drk01@users.noreply.github.com>
2021-08-15 06:57:42 -0500

    Restoring default nvchad config

--- lua/modules/autopairs.lua
+++ lua/modules/autopairs.lua
@@ -1,14 +1,14 @@
 local present1, autopairs = pcall(require, "nvim-autopairs")
-local present2, autopairs_completion = pcall(require, "nvim-autopairs.completion.compe")
+local present2, autopairs_completion = pcall(require,
+                                             "nvim-autopairs.completion.compe")
 
-if not (present1 or present2) then
-    return
-end
+if not (present1 or present2) then return end
 
-autopairs.setup{}
+autopairs.setup {}
 
-require("nvim-autopairs.completion.compe").setup({
-  map_cr = true, --  map <CR> on insert mode
-  map_complete = true, -- it will auto insert `(` after select function or method item
-  auto_select = true,  -- auto select first item
+autopairs_completion.setup({
+    map_cr = true,
+    map_complete = true,
+    auto_select = true
 })
+
