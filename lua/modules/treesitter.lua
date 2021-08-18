661aa9fa7b6dbeb689f5c30adf130c83bcf99128
Omar Monrroy <45756472+Drk01@users.noreply.github.com>
2021-08-18 17:37:09 -0500

    Formatting code

--- lua/modules/treesitter.lua
+++ lua/modules/treesitter.lua
@@ -1,19 +1,10 @@
 local present, ts_config = pcall(require, "nvim-treesitter.configs")
-if not present then
-    return
-end
+if not present then return end
 
 ts_config.setup {
-    ensure_installed = {
-        "bash",
-        "lua"
-    },
-    highlight = {
-        enable = true,
-        use_languagetree = true
-    },
-    autotag = {
-        enable = true
-    },
+    ensure_installed = {"bash", "lua"},
+    highlight = {enable = true, use_languagetree = true},
+    autotag = {enable = true},
+    autopairs = {enable = true}
 }
 
