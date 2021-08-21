eca9c6133e116f242b2d393d8c22fe882ffd2bfb
Omar Monrroy <45756472+Drk01@users.noreply.github.com>
2021-08-20 21:21:15 -0500

    Adding pears config
    Prevent duplicated pairs and adding compe support

--- lua/modules/autopairs.lua
+++ lua/modules/autopairs.lua
@@ -1,1 +1,18 @@
-require"pears".setup()
+local R = require "pears.rule"
+
+require"pears".setup(function(conf)
+    conf.expand_on_enter(true)
+    conf.on_enter(function(pears_handle)
+        if vim.fn.pumvisible() == 1 and vim.fn.complete_info().selected ~= -1 then
+            return vim.fn["compe#confirm"]("<CR>")
+        else
+            pears_handle()
+        end
+    end)
+
+    conf.pair("'", {
+        close = "'",
+        should_expand = R.all_of(R.not_(R.start_of_context "[a-zA-Z]"),
+                                 R.child_of_node "string")
+    })
+end)
