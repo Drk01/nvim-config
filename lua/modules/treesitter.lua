b0e4dfb747e6e7f256b7328b7af5c8def50945ed
Omar Monrroy <45756472+Drk01@users.noreply.github.com>
2021-09-21 05:17:42 -0500

    Adding basic requires for treesitter

--- lua/modules/treesitter.lua
+++ lua/modules/treesitter.lua
@@ -2,7 +2,11 @@ local present, ts_config = pcall(require, "nvim-treesitter.configs")
 if not present then return end
 
 ts_config.setup {
-    ensure_installed = {"mantained"},
+    ensure_installed = {
+        "bash", "lua", "php", "typescript", "graphql", "vue", "dockerfile",
+        "json", "yaml", "comment", "jsonc", "tsx", "html", "json5", "jsdoc",
+        "latex", "css"
+    },
     highlight = {enable = true, use_languagetree = true},
     autotag = {enable = true},
     autopairs = {enable = true}
