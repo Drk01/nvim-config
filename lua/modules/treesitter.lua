9a7150072618eaacf2054f7e179ac846da827cf6
Omar Monrroy <45756472+Drk01@users.noreply.github.com>
2021-09-21 04:07:26 -0500

    Adding support for all mantained treesitter syntax

--- lua/modules/treesitter.lua
+++ lua/modules/treesitter.lua
@@ -2,7 +2,7 @@ local present, ts_config = pcall(require, "nvim-treesitter.configs")
 if not present then return end
 
 ts_config.setup {
-    ensure_installed = {"bash", "lua"},
+    ensure_installed = {"mantained"},
     highlight = {enable = true, use_languagetree = true},
     autotag = {enable = true},
     autopairs = {enable = true}
