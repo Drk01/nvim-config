09b5acf52b4e459ad301dd18f37826191ce384f8
Omar Monrroy <45756472+Drk01@users.noreply.github.com>
2021-11-12 01:13:26 -0600

    Enabling TSLspOrganizeSync only for Typescript

--- init.lua
+++ init.lua
@@ -17,8 +17,8 @@ vim.g.lightline = {
     },
     component_function = {gitbranch = 'fugitive#head'}
 }
--- Format on save
--- vim.api.nvim_command("au BufWrite * :TSLspOrganizeSync")
+-- Format on save: Only works with Typescript 
+vim.api.nvim_command("au BufWrite * :TSLspOrganizeSync")
 vim.api.nvim_command("au BufWrite * :Neoformat")
 
 -- Vimspector
