5211f6f7139f51a2018d7fa62315791ae2cb9a22
Omar Monrroy <45756472+Drk01@users.noreply.github.com>
2021-12-26 17:01:48 -0600

    Formatting code

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
-require 'plugins'
+require "plugins"
 
 -- Colorscheme
 vim.o.termguicolors = true
@@ -11,16 +11,17 @@ vim.g.t_Co = 256
 vim.g.lightline = {
     active = {
         left = {
-            {'mode', 'paste'}, {'gitbranch', 'readonly', 'filename', 'modified'}
+            {"mode", "paste"},
+            {"gitbranch", "readonly", "filename", "modified"}
         }
     },
-    component_function = {gitbranch = 'fugitive#head'}
+    component_function = {gitbranch = "fugitive#head"}
 }
--- Format on save: Only works with Typescript 
+-- Format on save: Only works with Typescript
 vim.api.nvim_command("au BufWrite * :Neoformat")
 
 -- Vimspector
 vim.cmd [[ let g:vimspector_enable_mappings = 'VISUAL_STUDIO' ]]
 
-require 'mappings'
-require 'opts'
+require "mappings"
+require "opts"
