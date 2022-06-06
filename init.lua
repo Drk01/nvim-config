ec045a99ba16a8cce6e7921e426dbe08c3e94b81
Omar Monrroy <45756472+DrK01@users.noreply.github.com>
2022-06-06 11:09:43 -0500

    Fixing comment

--- init.lua
+++ init.lua
@@ -7,7 +7,7 @@ vim.cmd [[highlight link CompeDocumentation NormalFloat]]
 
 vim.g.t_Co = 256
 
--- Format on save: Only works with Typescript
+-- Format on save
 vim.api.nvim_command("au BufWrite * :Neoformat")
 
 require "mappings"
