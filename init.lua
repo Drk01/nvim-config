a60b237b7b9d6c5ef7a44424ef02ae1c8f1453db
Omar Monrroy <45756472+DrK01@users.noreply.github.com>
2022-08-24 20:11:35 -0500

    Removing useless code

--- init.lua
+++ init.lua
@@ -2,11 +2,7 @@ require "plugins"
 
 require("mason").setup()
 require("mason-lspconfig").setup()
-require("mason-lspconfig").setup_handlers {
-    function(server_name)
-        require("lspconfig")[server_name].setup {}
-    end
-}
+require("mason-lspconfig").setup_handlers {}
 
 -- Colorscheme
 vim.o.termguicolors = true
