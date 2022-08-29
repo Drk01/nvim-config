8fa12f0c122409997ef65196c3f709f9cd9146f8
Omar Monrroy <45756472+DrK01@users.noreply.github.com>
2022-08-28 20:25:31 -0500

    Adding lsp keymaps to all servers

--- init.lua
+++ init.lua
@@ -2,11 +2,6 @@ require "plugins"
 
 require("mason").setup()
 require("mason-lspconfig").setup()
-require("mason-lspconfig").setup_handlers {
-    function(server_name)
-        require("lspconfig")[server_name].setup {}
-    end
-}
 
 -- Colorscheme
 vim.o.termguicolors = true
