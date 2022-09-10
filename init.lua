24a0c71aed9133a7c11df5041b1bd2949ee7901e
Omar Monrroy <45756472+DrK01@users.noreply.github.com>
2022-09-10 07:31:41 -0500

    Modify foldexpr to fold imports

--- init.lua
+++ init.lua
@@ -9,6 +9,7 @@ vim.cmd [[colorscheme ayu]]
 vim.cmd [[highlight link CompeDocumentation NormalFloat]]
 vim.cmd [[set foldmethod=expr]]
 vim.cmd [[set foldexpr=nvim_treesitter#foldexpr()]]
+vim.cmd [[set foldexpr=v:lnum==1?'>1':getline(v:lnum)=~'import'?1:nvim_treesitter#foldexpr()]]
 
 vim.g.t_Co = 256
 
